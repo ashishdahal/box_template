1eb4a3bd1bb7d9e78723a41910bfea52d5db37df
Ashish Dahal <dahalasish1@gmail.com>
2021-06-15 14:55:00 -0500

    Modified prime container with style.css

--- data.js
+++ data.js
@@ -1,41 +1,45 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
-const box = {"color":"green"};
-const container = {"items":[{"type":box}]};
-let data = JSON.parse(JSON.stringify(container));
+//const box = {"color":"green"};
+//const container = {"items":[{"type":box}]};
+//let data = JSON.parse(JSON.stringify(container));
 
+const BuildButtonTemplate = `<button id="build" class = "build">build</button>`
 
-const CbuttonTemplate = `<button id="buildContainer" onclick="bldCon()">buildContainer</button>`
+const CbuttonTemplate = `<button id="buildContainer" class="buildContainer">buildContainer</button>`
 
-const BbuttonTemplate = `<button id="buildBox" onclick="bldBox()">BuildBOX</button>`
+const BbuttonTemplate = `<button id="buildBox" class="buildBox">buildBox</button>`
 
 const containerTemplate = `<div id="containerTemplate" class = "container">container
 </div>`
 
-const boxTemplate =`<div class = "box">box</div>`
+const boxTemplate =`<div id="boxTemplate" class = "box">box</div>`
 
-document.getElementById("prime").innerHTML += CbuttonTemplate;
+document.getElementById("prime").innerHTML += BuildButtonTemplate;
 
-
-
-function bldCon(){
-
-  document.getElementById("prime").innerHTML += CbuttonTemplate;
-  document.getElementById("prime").innerHTML += BbuttonTemplate;
+function buildButtons(){
   document.getElementById("prime").innerHTML += containerTemplate;
+  document.getElementById("containerTemplate").innerHTML += CbuttonTemplate;
+  document.getElementById("containerTemplate").innerHTML += BbuttonTemplate;
 
-};
+  function bldCon(){
+    document.getElementById("containerTemplate").innerHTML += containerTemplate;
+    
+    }
+    
+    document.getElementById("buildContainer").addEventListener("click", bldCon);
 
-function bldBox(){
- 
- 
-  document.getElementById("containerTemplate").innerHTML += boxTemplate;
 
-};
+    function bldBox(){
+     document.getElementById("containerTemplate").innerHTML += boxTemplate;
+    }
 
-document.getElementById("buildContainer").addEventListener("click", bldCon());
+    document.getElementById("buildBox").addEventListener("click", bldBox);
 
-document.getElementById("buildBox").addEventListener("click", bldBox());
+
+}
+
+    document.getElementById("build").addEventListener("click", buildButtons);
 
 
 
